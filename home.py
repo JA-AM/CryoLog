40635dc89715a8cb16a0411ccd9b8caacde6b7da
Azhou2023 <anz008@ucsd.edu>
2024-05-06 01:05:55 +0000

    session state + db integration

--- home.py
+++ home.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pyrebase
+from pages import login
 
-st.title("This is the Home Page")
+config = st.secrets['firebaseConfig']
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+if 'user' not in st.session_state:
+    st.switch_page("pages/login.py")
+
+currUser = st.session_state['user']
+username =  db.child("users").child(currUser['localId']).get().val()["Username"]
+st.title("Welcome, " + username)
 
